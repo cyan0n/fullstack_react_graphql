cfe424a93491d7682313aff23ebeb2f5e53f418d
Thomas <thomas.cipriani@esimple.it>
2021-04-01 16:35:45 +0200

    Created first entity Post

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,17 @@
-console.log("Hello, World!");
+import { MikroORM } from "@mikro-orm/core";
+import { __prod__ } from "./config";
+import { Post } from "./entities/Post";
+
+const main = async () => {
+  const orm = await MikroORM.init({
+    entities: [Post],
+    dbName: "lireddit",
+    type: "postgresql",
+    debug: !__prod__,
+  });
+
+  const post = orm.em.create(Post, { title: "my first post" });
+  await orm.em.persistAndFlush(post);
+};
+
+main().catch(console.error);
