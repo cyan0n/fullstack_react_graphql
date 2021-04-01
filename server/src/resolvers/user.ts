34b725ab72274a085abc5612fd5d5600c42a2160
Cyan0n <cyan0n.dev@gmail.com>
2021-04-01 18:25:51 +0200

    user registration and login

---
+++ server/src/resolvers/user.ts
@@ -0,0 +1,83 @@
+import { AppContext } from "../types";
+import {
+  Arg,
+  Ctx,
+  Field,
+  InputType,
+  Mutation,
+  ObjectType,
+  Resolver,
+} from "type-graphql";
+import argon2 from "argon2";
+import { User } from "../entities/User";
+
+@InputType()
+class UsernamePasswordInput {
+  @Field()
+  username: string;
+  @Field()
+  password: string;
+}
+
+@ObjectType()
+class FieldError {
+  @Field()
+  field: string;
+  @Field()
+  message: string;
+}
+@ObjectType()
+class UserResponse {
+  @Field(() => [FieldError], { nullable: true })
+  errors?: FieldError[];
+
+  @Field(() => User, { nullable: true })
+  user?: User;
+}
+
+@Resolver()
+export class UserResolver {
+  @Mutation(() => UserResponse)
+  async register(
+    @Arg("options") options: UsernamePasswordInput,
+    @Ctx() { em }: AppContext,
+  ): Promise<UserResponse> {
+    const passowrdHash = await argon2.hash(options.password);
+    const user = em.create(User, {
+      username: options.username,
+      password: passowrdHash,
+    });
+    await em.persistAndFlush(user);
+    return { user };
+  }
+
+  @Mutation(() => UserResponse)
+  async login(
+    @Arg("options") options: UsernamePasswordInput,
+    @Ctx() { em }: AppContext,
+  ): Promise<UserResponse> {
+    const user = await em.findOne(User, { username: options.username });
+    if (!user) {
+      return {
+        errors: [
+          {
+            field: "username",
+            message: "username/password incorrect",
+          },
+        ],
+      };
+    }
+    const valid = await argon2.verify(user.password, options.password);
+    if (!valid) {
+      return {
+        errors: [
+          {
+            field: "username",
+            message: "username/password incorrect",
+          },
+        ],
+      };
+    }
+    return { user };
+  }
+}
