4f86e7afa28aa2bf79c2c475bdcb257d648d9496
Thomas <thomas.cipriani@esimple.it>
2021-04-01 17:03:49 +0200

    mikro orm setup finished

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,14 +1,11 @@
 import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./config";
 import { Post } from "./entities/Post";
+import mikroConfig from "./mikro-orm.config";
 
 const main = async () => {
-  const orm = await MikroORM.init({
-    entities: [Post],
-    dbName: "lireddit",
-    type: "postgresql",
-    debug: !__prod__,
-  });
+  const orm = await MikroORM.init(mikroConfig);
+  await orm.getMigrator().up();
 
   const post = orm.em.create(Post, { title: "my first post" });
   await orm.em.persistAndFlush(post);
