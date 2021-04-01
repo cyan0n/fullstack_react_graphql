d34e7a56a901a4b68733825669c2bf462cbb479e
Cyan0n <cyan0n.dev@gmail.com>
2021-04-01 17:54:06 +0200

    Post crud

---
+++ server/src/resolvers/post.ts
@@ -0,0 +1,54 @@
+import { AppContext } from "../types";
+import { Arg, Ctx, Mutation, Query, Resolver } from "type-graphql";
+import { Post } from "../entities/Post";
+
+@Resolver()
+export class PostResolver {
+  @Query(() => [Post])
+  posts(@Ctx() { em }: AppContext): Promise<Post[]> {
+    return em.find(Post, {});
+  }
+
+  @Query(() => Post, { nullable: true })
+  post(@Arg("id") id: number, @Ctx() { em }: AppContext): Promise<Post | null> {
+    return em.findOne(Post, { id });
+  }
+
+  @Mutation(() => Post)
+  async createPost(
+    @Arg("title") title: string,
+    @Ctx() { em }: AppContext,
+  ): Promise<Post> {
+    const post = em.create(Post, { title });
+    await em.persistAndFlush(post);
+    return post;
+  }
+
+  @Mutation(() => Post, { nullable: true })
+  async updatePost(
+    @Arg("id") id: number,
+    @Arg("title", () => String, { nullable: true }) title: string,
+    @Ctx() { em }: AppContext,
+  ): Promise<Post | null> {
+    const post = await em.findOne(Post, { id });
+    if (!post) return null;
+    if (typeof title !== "undefined") {
+      post.title = title;
+      await em.persistAndFlush(post);
+    }
+    return post;
+  }
+
+  @Mutation(() => Boolean)
+  async deletePost(
+    @Arg("id") id: number,
+    @Ctx() { em }: AppContext,
+  ): Promise<boolean> {
+    try {
+      await em.nativeDelete(Post, { id });
+    } catch {
+      return false;
+    }
+    return true;
+  }
+}
