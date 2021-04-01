5c912e2d07e7b3a89cf3e45d02b331a418445990
Thomas <thomas.cipriani@esimple.it>
2021-04-01 17:15:47 +0200

    Initialize Apollo and graphql

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,14 +1,29 @@
 import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./config";
-import { Post } from "./entities/Post";
 import mikroConfig from "./mikro-orm.config";
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { HelloResolver } from "./resolvers/hello";
 
 const main = async () => {
   const orm = await MikroORM.init(mikroConfig);
   await orm.getMigrator().up();
 
-  const post = orm.em.create(Post, { title: "my first post" });
-  await orm.em.persistAndFlush(post);
+  const app = express();
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver],
+      validate: false,
+    }),
+  });
+
+  apolloServer.applyMiddleware({ app });
+
+  app.listen(4000, () => {
+    console.log("server started on http://localhost:4000");
+  });
 };
 
 main().catch(console.error);
