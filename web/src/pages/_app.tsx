f491e00d21435ac70aceb47f6af67124c5da6853
Cyan0n <cyan0n.dev@gmail.com>
2021-04-04 21:14:04 +0200

    connected user registration to server GrahQL API

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -1,19 +1,27 @@
 import { ChakraProvider, ColorModeProvider } from "@chakra-ui/react";
+import { Provider, createClient } from "urql";
 
 import theme from "../theme";
 
-function App({ Component, pageProps }) {
+const client = createClient({
+  url: "http://localhost:4000/graphql",
+  fetchOptions: { credentials: "include" },
+});
+
+function App({ Component, pageProps }: any) {
   return (
-    <ChakraProvider resetCSS theme={theme}>
-      <ColorModeProvider
-        options={{
-          initialColorMode: "dark",
-          useSystemColorMode: false,
-        }}
-      >
-        <Component {...pageProps} />
-      </ColorModeProvider>
-    </ChakraProvider>
+    <Provider value={client}>
+      <ChakraProvider resetCSS theme={theme}>
+        <ColorModeProvider
+          options={{
+            initialColorMode: "dark",
+            useSystemColorMode: false,
+          }}
+        >
+          <Component {...pageProps} />
+        </ColorModeProvider>
+      </ChakraProvider>
+    </Provider>
   );
 }
 
--- web/src/pages/register.tsx
+++ web/src/pages/register.tsx
@@ -9,15 +9,28 @@ import {
 } from "@chakra-ui/react";
 import { Container } from "../components/Container";
 import { InputField } from "../components/InputField";
+import { useMutation } from "urql";
+import { useRegisterMutation } from "../generated/graphql";
+import { toErrorMap } from "../utils/toErrorMap";
+import { useRouter } from "next/router";
 
 interface RegisterProps {}
 
 const Register: React.FC<RegisterProps> = ({}) => {
+  const router = useRouter();
+  const [, register] = useRegisterMutation();
   return (
     <Container variant="small">
       <Formik
         initialValues={{ username: "", password: "" }}
-        onSubmit={console.log}
+        onSubmit={async (values, { setErrors }) => {
+          const response = await register(values);
+          if (response.data?.register.errors) {
+            setErrors(toErrorMap(response.data.register.errors));
+          } else if (response.data?.register.user) {
+            router.push("/");
+          }
+        }}
       >
         {({ isSubmitting }) => (
           <Form>
